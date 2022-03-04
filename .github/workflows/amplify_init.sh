ab153ff48d48e4cdad88449844d07596e43ac32f
Tommy Nam <tommy@orcabase.com>
2022-03-04 15:06:38 +0800

    feat: Add project name config arg to bash script

---
+++ .github/workflows/amplify_init.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+IFS='|'
+
+AMPLIFY="{\
+\"projectName\":\"prism_flutter\",\
+\"envName\":\"dev\",\
+\"defaultEditor\":\"code\"\
+}"
+
+amplify init \
+—- amplify $AMPLIFY \
+-- frontend $FRONTEND \
+-- providers $PROVIDERS \
+-- yes
